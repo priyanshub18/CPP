f39ed7f0e2ac5ad780855d637ad7cb8eba5d6543
priyanshub18 <priyanshubhardwaj658@gmail.com>
2024-04-14 00:39:53 +0530

    main

--- DYNAMIC_PROGRAMMING/ninja_training.cpp
+++ DYNAMIC_PROGRAMMING/ninja_training.cpp
@@ -27,8 +27,30 @@ int f(vector<vector<int>> &points,int day , int last,vector<vector<int>> & dp){
 int ninjaTraining(int n, vector<vector<int>> &points)
 {
 
-    vector<vector<int> > dp(points.size() +1 , vector<int>(4 , -1));
-    return f(points,n-1 , 3,dp);
+    vector<vector<int> > dp(points.size() +1 , vector<int>(4 , 0));
+    dp[0][0] = max(points[0][1] , points[0][2]);
+    dp[0][1] = max(points[0][0] , points[0][2]);
+    dp[0][2] = max(points[0][1] , points[0][1]);
+    dp[0][3] = max(points[0][1] , max(points[0][1],points[0][2]));
+
+    for(int day = 1 ; day < n ;day++){
+        for(int last = 0 ; last < 4 ; last++){
+            dp[day][last] = 0;
+            int maxi = 0;
+
+            for(int task = 0 ; task < 3 ; task++){
+                if(task != last){
+                    int point = points[day][task] + dp[day-1][task];
+                    maxi = max(maxi , point);
+
+                }
+            }
+       
+        }
+    }
+    return dp[n-1][3];
+
+    //return f(points,n-1 , 3,dp);
 }                     
 int main(){
     
