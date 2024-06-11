0031984939a6ea1230b8237f5d26a08eb40d45e8
priyanshub18 <priyanshubhardwaj658@gmail.com>
2024-06-11 15:01:50 +0530

    main

--- BitManupulation/single_number2.cpp
+++ BitManupulation/single_number2.cpp
@@ -2,21 +2,21 @@
 #include<vector>
                     
 using namespace std;
-class Solution {
-public:
-    int singleNumber(vector<int>& nums) {
-       int ans = 0;
-       for(int bit = 0 ; bit < 32 ; bit++){
-            int cnt = 0;
-            for(int i = 0 ; i < nums.size(); i++){
-                if(nums[i] & (1 << bit)) cnt++;
-            }
-            if(cnt % 3 == 1) ans = ans | (1 << bit);//to set the bit 
-       } 
+// class Solution {
+// public:
+//     int singleNumber(vector<int>& nums) {
+//        int ans = 0;
+//        for(int bit = 0 ; bit < 32 ; bit++){
+//             int cnt = 0;
+//             for(int i = 0 ; i < nums.size(); i++){
+//                 if(nums[i] & (1 << bit)) cnt++;
+//             }
+//             if(cnt % 3 == 1) ans = ans | (1 << bit);//to set the bit 
+//        } 
 
-       return ans;
-    }
-};
+//        return ans;
+//     }
+// };
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
